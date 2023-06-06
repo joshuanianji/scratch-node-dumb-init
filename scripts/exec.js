3e82eb5fb725552debf62e7dfb2f24ca90f01d3b
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-06 22:29:51 +0000

    Add JSDoc to exec

--- scripts/exec.js
+++ scripts/exec.js
@@ -1,14 +1,27 @@
 const { execFileSync } = require('node:child_process');
 
-// https://github.com/vercel/vercel/blob/9890255dcfec743274a3521b603a67eda3f5fca0/utils/exec.js
-function exec(cmd, args, opts) {
+/**
+ * Executes a command in bash.
+ * Using JSDoc for type interence poggers
+ *
+ * ttps://github.com/vercel/vercel/blob/9890255dcfec743274a3521b603a67eda3f5fca0/utils/exec.js
+ *
+ * @param {string} cmd
+ * @param {string[]} args
+ * @param {boolean | undefined} logOutput
+ * @param {any} opts
+ * @returns {string} output
+ */
+function exec(cmd, args, logOutput, opts) {
     console.log({ input: `${cmd} ${args.join(' ')}` });
     const output = execFileSync(cmd, args, {
         encoding: 'utf-8',
         ...opts,
     }).trim();
-    console.log({ output });
-    console.log();
+    if (logOutput) {
+        console.log({ output });
+        console.log();
+    }
     return output;
 }
 
