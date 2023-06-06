1f53753d70924b8b87fe761d5fb30c99a546d9af
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-06 22:30:15 +0000

    Parse docker image ls table, collect tags

--- scripts/update-versions.js
+++ scripts/update-versions.js
@@ -12,12 +12,55 @@ module.exports = async ({ github, context, core }) => {
     const parentPath = '/tmp/artifacts';
     const contents = await fs.readdir(parentPath);
     for (const folder of contents) {
-        exec('docker', [
-            'load',
-            '--input',
-            `${parentPath}/${folder}/${folder}.tar`,
-        ]);
+        exec(
+            'docker',
+            ['load', '--input', `${parentPath}/${folder}/${folder}.tar`],
+            true
+        );
     }
 
-    exec('docker', ['image', 'ls', '-a']);
+    const output = exec(
+        'docker',
+        [
+            'image',
+            'ls',
+            '-a',
+            '--format',
+            '{{.Repository}}\t{{.Tag}}\t{{.Size}}\t{{.ID}}',
+        ],
+        false
+    );
+
+    // create some data structures to hold the image information
+    // note we want the images to be sorted in descending order according to the node version
+    const images = new Map();
+
+    // skip the first line
+    for (const line of output.split('\n').slice(1)) {
+        [repo, tag, size, id] = line.split('\t');
+        if (repo != 'mcr.microsoft.com/devcontainers/miniconda') {
+            continue;
+        }
+
+        const meta = images.get(id);
+        if (meta == undefined) {
+            images.set(id, { tags: [tag], size: size });
+        } else {
+            images.set(id, { tags: [tag, ...meta.tags], size: size });
+        }
+    }
+
+    console.log(images);
+
+    for (const [id, meta] of images.entries()) {
+        meta.tags.sort((a, b) => b.length - a.length);
+    }
+
+    console.log(images);
+
+    const metadatas = Array.from(images.entries()).map(([id, meta]) => meta);
+    metadatas.sort(
+        (a, b) => Number.parseInt(a.tags[0]) - Number.parseInt(b.tags[0])
+    );
+    console.log(metadatas);
 };
