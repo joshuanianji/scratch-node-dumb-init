2679ed5ff255817d7c5c61de5ef32a2413727199
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-07 16:22:34 +0000

    Take 3 init

--- scripts/update-versions.js
+++ scripts/update-versions.js
@@ -7,60 +7,55 @@ const { exec } = require('./exec');
  * @returns
  */
 module.exports = async ({ github, context, core }) => {
-    // we first load all the artifacts into docker using docker --load
-    // artifacts are located in /tmp/artifacts/image-{NODE_VERSION}/image-{NODE_VERSION}.tar
-    const parentPath = '/tmp/artifacts';
-    const contents = await fs.readdir(parentPath);
-    for (const folder of contents) {
-        exec(
+    const data = [];
+    let counter = 0;
+    let page_idx = 1;
+
+    // get all versions and their tags
+    // to do this, we iterate through ALL the package versions (a lot of SHA hashes)
+    // while finding those versions with tags
+    // this ASSUMES that we care about all the tags
+    while (true) {
+        const { data: versions } =
+            await github.rest.packages.getAllPackageVersionsForPackageOwnedByAuthenticatedUser(
+                {
+                    username: 'joshuanianji',
+                    package_name: 'scratch-node-dumb-init',
+                    package_type: 'container',
+                    per_page: 100,
+                    page: page_idx,
+                }
+            );
+
+        if (versions.length == 0) {
+            break;
+        }
+
+        for (const version of versions) {
+            const tags = version.metadata.container?.tags;
+            if (tags && tags.length > 0) {
+                console.log(`Version ${version.name} has tags:\n\t${tags}`);
+                data.push({
+                    name: version.name,
+                    tags: tags,
+                });
+            }
+        }
+
+        counter += versions.length;
+        page_idx += 1;
+    }
+
+    console.log(`Looked at ${counter} versions and ${page_idx} pages`);
+    console.log(data);
+
+    for (const datum of data) {
+        const imageName = `ghcr.io/joshuanianji/scratch-node-dumb-init:${datum.tags[0]}`;
+        exec('docker', ['pull', imageName], true);
+        const size = exec(
             'docker',
-            ['load', '--input', `${parentPath}/${folder}/${folder}.tar`],
+            ['image', 'inspect', imageName, '--format', '{{.Size}}'],
             true
         );
     }
-
-    const output = exec(
-        'docker',
-        [
-            'image',
-            'ls',
-            '-a',
-            '--format',
-            '{{.Repository}}\t{{.Tag}}\t{{.Size}}\t{{.ID}}',
-        ],
-        false
-    );
-
-    // create some data structures to hold the image information
-    // note we want the images to be sorted in descending order according to the node version
-    const images = new Map();
-
-    // skip the first line
-    for (const line of output.split('\n').slice(1)) {
-        [repo, tag, size, id] = line.split('\t');
-        if (repo != 'mcr.microsoft.com/devcontainers/miniconda') {
-            continue;
-        }
-
-        const meta = images.get(id);
-        if (meta == undefined) {
-            images.set(id, { tags: [tag], size: size });
-        } else {
-            images.set(id, { tags: [tag, ...meta.tags], size: size });
-        }
-    }
-
-    console.log(images);
-
-    for (const [id, meta] of images.entries()) {
-        meta.tags.sort((a, b) => b.length - a.length);
-    }
-
-    console.log(images);
-
-    const metadatas = Array.from(images.entries()).map(([id, meta]) => meta);
-    metadatas.sort(
-        (a, b) => Number.parseInt(a.tags[0]) - Number.parseInt(b.tags[0])
-    );
-    console.log(metadatas);
 };
