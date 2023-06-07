bb78e57f512994b75b7a6fd0effa9ed47ea64f6d
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-07 16:36:29 +0000

    Edit markdown in METADATA.md

--- scripts/update-versions.js
+++ scripts/update-versions.js
@@ -49,6 +49,8 @@ module.exports = async ({ github, context, core }) => {
     console.log(`Looked at ${counter} versions and ${page_idx} pages`);
     console.log(data);
 
+    let markdown = `| Tags | Size | Architectures |\n| ---- | ---- | ------------- |\n`;
+
     for (const datum of data) {
         const imageName = `ghcr.io/joshuanianji/scratch-node-dumb-init:${datum.tags[0]}`;
         exec('docker', ['pull', imageName], true);
@@ -57,5 +59,38 @@ module.exports = async ({ github, context, core }) => {
             ['image', 'inspect', imageName, '--format', '{{.Size}}'],
             true
         );
+        markdown += `| ${datum
+            .map((a) => `\`${a}\``)
+            .join(' ')} | ${formatBytes(size)} | \`amd64\` \`arm64\``;
     }
+    console.log('METADATA:', metadata);
+
+    // edit METADATA.md with markdown file
+    console.log('Editing METADATA.md');
+    await fs.writeFile('METADATA.md', markdown);
+    console.log('Write file: showing contents');
+    exec('cat', ['MARKDOWN.md']);
 };
+
+// https://stackoverflow.com/a/18650828
+function formatBytes(bytes, decimals = 2) {
+    if (!+bytes) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = [
+        'Bytes',
+        'KiB',
+        'MiB',
+        'GiB',
+        'TiB',
+        'PiB',
+        'EiB',
+        'ZiB',
+        'YiB',
+    ];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
